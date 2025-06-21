3495b59c1f0505817319f51658ca80ee087e5447
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-06-21 19:16:08 -0300

    Uso do while para selecionar candidatos

--- controle-canditados/src/candidatura/ProcessoSeletivo.java
+++ controle-canditados/src/candidatura/ProcessoSeletivo.java
@@ -1,14 +1,34 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidatos(1900.0);
-        analisarCandidatos(2100.0);
-        analisarCandidatos(2000.0);
-        
+        selecaoCandidatos();
     }
 
-    public static void analisarCandidatos(double salarioPretendido){
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Jaqueline","Joana", "Carlos", "Miguel", "Ana", "Pedro", "Mariana", "Rafael", "Sofia", "Lucas", "Isabela"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        
+        while(candidatosSelecionados < 5 && candidatoAtual <= candidatos.length){
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato "+candidatos[candidatoAtual]+" pretende o salário "+salarioPretendido);
+            if (salarioPretendido <= 2000){
+                System.out.println("O candidato(a) "+candidatos[candidatoAtual]+" foi selecionado(a).");
+                candidatosSelecionados ++;
+            }
+            candidatoAtual ++;
+        }
+
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
+    static void analisarCandidatos(double salarioPretendido){
         double salarioBase = 2000.0;
 
         if (salarioBase > salarioPretendido){
