777473c127a51322e8d5bb270d9eb928ec8b30be
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-06-22 19:50:34 -0300

    projeto finalizado

--- controle-canditados/src/candidatura/ProcessoSeletivo.java
+++ controle-canditados/src/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,7 @@
 package candidatura;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
@@ -12,16 +14,28 @@ public class ProcessoSeletivo {
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
         
-        while(candidatosSelecionados < 5 && candidatoAtual <= candidatos.length){
+        ArrayList<String> candidatosSelecionadosLista = new ArrayList<>();
+
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
             double salarioPretendido = valorPretendido();
             System.out.println("O candidato "+candidatos[candidatoAtual]+" pretende o salário "+salarioPretendido);
             if (salarioPretendido <= 2000){
                 System.out.println("O candidato(a) "+candidatos[candidatoAtual]+" foi selecionado(a).");
                 candidatosSelecionados ++;
+                candidatosSelecionadosLista.add(candidatos[candidatoAtual]);
             }
             candidatoAtual ++;
         }
 
+        imprimindoSelecionados(candidatosSelecionadosLista);
+        entrandoEmContato(candidatosSelecionadosLista);
+
+    }
+
+    static void imprimindoSelecionados( ArrayList<String> candidatosSelecionadosLista){
+        for (int i = 0; i < candidatosSelecionadosLista.size(); i++ ){
+            System.out.println("Índice: "+(i+1)+" Candidato: "+ candidatosSelecionadosLista.get(i));
+        }
     }
 
     static double valorPretendido(){
@@ -30,7 +44,6 @@ public class ProcessoSeletivo {
 
     static void analisarCandidatos(double salarioPretendido){
         double salarioBase = 2000.0;
-
         if (salarioBase > salarioPretendido){
             System.out.println("Ligar para o candidato");
         }else if(salarioBase == salarioPretendido){
@@ -38,5 +51,27 @@ public class ProcessoSeletivo {
         }else{
             System.out.println("Aguardar demais candidatos");
         }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+    static void entrandoEmContato( ArrayList<String> candidatosSelecionadosLista){
+            for (int i = 0; i < candidatosSelecionadosLista.size(); i++){
+                int tentativas = 0;
+                boolean atendido = false;
+                while(!atendido){
+                    tentativas++;
+                    atendido = atender();
+                }
+
+                if (tentativas <= 3){
+                    System.out.println("O candidato "+candidatosSelecionadosLista.get(i)+" atendeu."+" Tentantiva: "+tentativas);
+
+                }else{
+                    System.out.println("O candidato "+candidatosSelecionadosLista.get(i)+" não atendeu");
+                }
+            }
     }
 }
